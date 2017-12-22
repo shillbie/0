44eb958403dda32d85d4141428c4b2eda6712e33
shillbie <33694875+shillbie@users.noreply.github.com>
2017-12-21 20:10:38 -0800

    Update ps.py

--- ps.py
+++ ps.py
@@ -6,11 +6,11 @@ from datetime import datetime
 import time,random,sys,json,codecs,threading,glob,re,os,subprocess
 
 cl = LINETCR.LINE()
-cl.login(token="EoE1da4Uvb1b.w6+8YbOkbOMyiHAGbC+hcW.uyMjf/ba1LdxPD2q91MGgn8NMtvJwCX3Nb7WFHHUxf0=")
+cl.login(token="EoZV6IODywAw6TJx984b.w6+8YbOkbOMyiHAGbC+hcW.jnCwtfBmM0U6koPtElYvX5CnmdtovF/JoEwRp+5LGTs=")
 cl.loginResult()
 
 ki = LINETCR.LINE() # Assist
-ki.login(token="EoAoBObO5FF3z3.m5X5B34I0MLNJSqaZLZ5uW.NJk9F7ck1Nq2pJSgaLJ0EuAloZiD7KorhoZBOAKwAzU=")
+ki.login(token="EoCPdoNFEkaPJa8bXDJ3.m5X5B34I0MLNJSqaZLZ5uW.YOpns1lxnjQcAW2ZBnu2JzBHoetAGXB8k1h5iTd58/M=")
 ki.loginResult()
 
 print "===[Login Success]==="
