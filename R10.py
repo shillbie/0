51b979391d1a22144fa57fd8835e33e82c0462fa
shillbie <33694875+shillbie@users.noreply.github.com>
2017-12-26 20:54:29 -0800

    Update R10.py

--- R10.py
+++ R10.py
@@ -1,9 +1,18 @@
 # -*- coding: utf-8 -*-
 
 import LINETCR
+import wikipedia
+import urllib
+import subprocess
+import profile
+import requests
 from LINETCR.lib.curve.ttypes import *
 from datetime import datetime
-import time,random,sys,json,codecs,threading,glob,re,ast,os,subprocess,requests
+import time,random,sys,json,codecs,threading,glob,re
+from bs4 import BeautifulSoup
+from threading import Thread
+from gtts import gTTS
+import urllib3
 
 cl = LINETCR.LINE() 
 #cl.login(qr=True)
